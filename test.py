9df34f18e8140ee9ae62ed1179ccd1ea1c8e670a
longhuarst <841105197@qq.com>
2019-01-14 00:29:14 +0800

    123

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+#coding=utf-8
+
 import ngram
 
 
@@ -14,3 +16,33 @@ print(ngram.NGram.compare("hello world", "nihao world",N=2))
 
 
 
+lst_str1 = str1.split(" ");
+
+
+print("lst_str1.len = %d"%len(lst_str1))
+print(lst_str1)
+
+
+lst_str2 = str2.split(" ");
+
+
+print("lst_str2.len = %d"%len(lst_str2))
+print(lst_str2)
+
+N = min(len(lst_str1),len(lst_str2))
+
+
+
+print("N=min(len(lst_str1),len(lst_str2)) = %d"%N)
+
+if N > 0 :
+    print("N>0")
+
+    #开始组词
+    #根据N组词
+
+    
+
+
+else :
+    print("N<=0")
